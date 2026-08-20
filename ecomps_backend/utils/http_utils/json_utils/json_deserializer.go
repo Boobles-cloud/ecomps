c04b3a8872ea22f8f873e2a7cdc7a4edcf823783
Mikel-217 <mikel.kunze@protonmail.com>
2026-08-20 15:04:22 +0200

    (wip) Added http and json utils

---
+++ ecomps_backend/utils/http_utils/json_utils/json_deserializer.go
@@ -0,0 +1,30 @@
+package jsonutils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Decodes a json http reqest body to a struct
+func JsonDeserilizeHttpRequest[T any](r *http.Request) (T, error) {
+
+	var v T
+
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		return v, err
+	}
+
+	return v, nil
+}
+
+// Decodes a byte slice to a given struct
+func JsonDeserilizeBytes[T any](d []byte) (T, error) {
+
+	var v T
+
+	if err := json.Unmarshal(d, &v); err != nil {
+		return v, err
+	}
+
+	return v, nil
+}
